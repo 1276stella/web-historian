7dfa77dd809486ed040e1fbfd72c5c9c073cad6b
MKS-Student <mks.student@hackreactor.com>
2015-10-08 18:25:57 -0700

    added cron to run the script every minute

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,10 +1,10 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
-var path = require('path');
 var archive = require('../helpers/archive-helpers');
-var httpHelpers = require('../web/http-helpers');
-var fs = require('fs');
-var request = require('request');
+var crontab = require('node-crontab');
 
-// var urls = archive.readListOfUrls(); 
-// archive.downloadUrls(urls);
+var jobId = crontab.scheduleJob("*/1 * * * *", function(){
+  var urls = archive.readListOfUrls(); 
+  archive.downloadUrls(urls);  
+  console.log('1 minute has passed');
+});
